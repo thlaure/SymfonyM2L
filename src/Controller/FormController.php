28563a5bbd3616cb8546e06eba92fbd7cbef0a35
Thomas L <thomaslaure3@gmail.com>
2018-04-14 17:56:56 +0200

    Réalisation du formulaire.

---
+++ src/Controller/FormController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Thomas LAURE
+ * Date: 14/04/2018
+ * Time: 16:56
+ */
+
+namespace App\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class FormController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_homepage")
+     */
+    public function homepage()
+    {
+        return $this->render('formulaire.html.twig');
+    }
+}
